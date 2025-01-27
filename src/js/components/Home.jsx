4fad1c5f7b4da7ead98246a31507ef96454b2d95
Oscar V <oscarvillalobosf@gmail.com>
2025-01-27 00:38:38 +0000

    first version of the traffic light with react + button

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-            
+// Create your first component
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+	function TrafficLight ()  {
+
+		/* Creo la constante que sera para uso de los colores*/
+		const [color, setColor] = useState("red");
+
+		/* Creo la constante que tendra el IF para el clic del boton*/
+		const clic= () => {
+			if (color === "red") setColor("yellow");
+			else if (color === "yellow") setColor("green");
+			else setColor("red");
+		  };
+		  	
+
+    return (
+					
+			
+			<div className="support">	
+				<div className="app d-inline-flex flex-column flex-nowrap"> 
+					<div  onClick={() => setColor("red")}    className={color === "red"    ? "red light" :     "red" }  	>		</div>
+					<div  onClick={() => setColor("yellow")} className={color === "yellow" ? "yellow light ":  "yellow" } 	>		</div>
+					<div  onClick={() => setColor("green")}  className={color === "green"  ? "green  light":	"green" } 	>		</div>
+				</div>
+
+				<div class="btn-group" role="group" aria-label="Basic example">
+
+					<button type="button" class="btn btn-primary"onClick={clic}>Cambiar Color</button>
+			
+				</div>
+		
+			</div> 
+
+
+		
+
+			
+
+	)
+
+		
 };
 
-export default Home;
+export default TrafficLight;
